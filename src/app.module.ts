813235ca0dcb1dd4728c322633b51ccdc5ea4f00
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-06-01 14:35:31 -0300

    feat: add mysql integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { Usuario } from './usuario/Usuario';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'cidade25',
+      database: 'cadastro',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Usuario]),
+    UsuarioModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/usuario/Usuario.ts
+++ src/usuario/Usuario.ts
@@ -1,9 +1,39 @@
-export class Usuario {
-  id: number;
+import { Table, Model, Column, DataType } from 'sequelize-typescript';
+
+@Table
+export class Usuario extends Model<Usuario> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   firstName: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   lastName: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   adress: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
   active: boolean;
+
+  @Column({
+    type: DataType.STRING,
+  })
   celphone: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   email: string;
 }
